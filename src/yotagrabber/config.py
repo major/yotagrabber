adc2c34ed0e7c899c95f50283a8a713732dbe956
Major Hayden <major@mhtx.net>
2023-04-21 14:41:47 -0500

    Import code

---
+++ src/yotagrabber/config.py
@@ -0,0 +1,36 @@
+"""Common configuration items used by the application."""
+import json
+import pathlib
+import random
+
+BASE_DIRECTORY = pathlib.Path(__file__).parent.resolve()
+
+
+def random_user_agent():
+    """Choose a user agent from a list of commonly used agents."""
+    with open(f"{BASE_DIRECTORY}/data/common_user_agents.json", "r") as fileh:
+        user_agents = json.load(fileh)
+
+    random.shuffle(user_agents)
+    return [x["ua"] for x in user_agents][0]
+
+
+def random_zip_code():
+    """Choose a random zip code from a list of zip codes."""
+    with open(f"{BASE_DIRECTORY}/data/zipcodes.txt", "r") as fileh:
+        zip_codes = fileh.readlines()
+
+    random.shuffle(zip_codes)
+    return [x for x in zip_codes][0].strip()
+
+
+def get_headers():
+    """Get the headers used when making requests to the Toyota website."""
+    headers = {
+        "origin": "https://www.toyota.com/",
+        "referrer": "https://www.toyota.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "user-agent": random_user_agent(),
+        "accept": "*/*",
+    }
+    return headers
---
+++ src/yotagrabber/models.py
@@ -0,0 +1,72 @@
+"""Get a list of Toyota models from the Toyota website."""
+import json
+
+import pandas as pd
+from python_graphql_client import GraphqlClient
+
+import config
+
+# Set to True to use local data and skip requests to the Toyota website.
+USE_LOCAL_DATA_ONLY = True
+
+
+def get_models_query():
+    """Read models query from a file."""
+    with open(f"{config.BASE_DIRECTORY}/graphql/models.graphql", "r") as fileh:
+        query = fileh.read()
+
+    # Replace the zip code with a random zip code.
+    query.replace("ZIPCODE", config.random_zip_code())
+
+    return query
+
+
+def read_local_data():
+    """Read local raw data from the disk instead of querying Toyota."""
+    with open("output/models_raw.json", "r") as fileh:
+        result = json.load(fileh)
+
+    return result
+
+
+def query_toyota():
+    """Query Toyota for a list of models."""
+    client = GraphqlClient(
+        endpoint="https://api.search-inventory.toyota.com/graphql",
+        headers=config.get_headers(),
+    )
+
+    result = client.execute(query=get_models_query())
+
+    with open("output/models_raw.json", "w") as fileh:
+        fileh.write(json.dumps(result, indent=2))
+
+    return result
+
+
+def generate_models():
+    """Generate a JSON file containing Toyota models."""
+    result = read_local_data() if USE_LOCAL_DATA_ONLY else query_toyota()
+
+    # Get the models from the result.
+    models = result["data"]["models"]
+    df = pd.json_normalize(models)
+
+    # Build a view and write it out as JSON.
+    models = (
+        df[
+            [
+                "modelCode",
+                "title",
+                "image",
+            ]
+        ]
+        .sort_values("modelCode", ascending=True)
+        .reset_index(drop=True)
+    )
+    models.to_json("output/models.json", orient="records", indent=2)
+    return models
+
+
+if __name__ == "__main__":
+    print(generate_models())
